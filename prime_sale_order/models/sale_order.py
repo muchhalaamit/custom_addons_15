9bdd08cca0489d96ebd79bfd9fceb596cb8a50b7
muchhalaamit <amit.muchhala@icreativetechnolabs.com>
2023-06-20 19:10:18 +0530

    [UPDATED]

--- prime_sale_order/models/sale_order.py
+++ prime_sale_order/models/sale_order.py
@@ -1,9 +1,29 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, api
+from odoo import fields, models
+from odoo.exceptions import ValidationError
 
 
 class SaleOrder(models.Model):
 	_inherit = "sale.order"
 
-	is_prime_sale = fields.Boolean(string="Prime Sale Order", default=False, copy=False)
+	is_prime_sale = fields.Boolean(string="Prime Sale Order",
+			default=False,
+			copy=False,
+			tracking=True,
+			readonly=True,
+			help="To create a prime sale order")
+
+	def set_prime_sale(self):
+		print("\n\n\n", dict(self._fields.state.items()))
+
+		if self.state in ('draft', 'sent'):
+			self.is_prime_sale = True
+		else:
+			raise ValidationError(f"This order can't be a prime sale order as it is in '{self.state}' state.")
+
+	def clear_prime_sale(self):
+		if self.state in ('draft', 'sent'):
+			self.is_prime_sale = False
+		else:
+			raise ValidationError(f"This action can't be done as it is in '{self.state}' state.")
